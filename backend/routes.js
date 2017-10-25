8a901b56fe647f0bd29d193d520374e7b933d2b8
zhaozhisheng <zhaozhisheng@aiyoumi.com>
2017-10-25 20:16:54 +0800

    update  更新

--- backend/routes.js
+++ backend/routes.js
@@ -1,7 +1,9 @@
 var express = require('express');
-var url = require('url');
-
 var router = express.Router();
+var bodyParser = require('body-parser')
+
+// create application/json parser
+var jsonParser = bodyParser.json()
 
 var interfaceController = require('./controller/interface.js');
 var recordsController = require('./controller/records.js');
@@ -9,18 +11,22 @@ var jsonRecordsController = require('./controller/jsonrecord.js');
 var userController = require('./controller/user.js');
 var projectController = require('./controller/project.js');
 
-router.post('/paramsSave', recordsController.addRecords);
-router.post('/getIterParamsByIterId', recordsController.getRecordsByInterID);
-
+/* Interface */
 router.post('/getInterfaceList', interfaceController.getAll);
 router.post('/getInterfaceListByProjectID', interfaceController.getRecordsByPID);
 router.post('/addInterface', interfaceController.addInterface);
+router.post('/updateInterface', interfaceController.updateInterface);
 router.get('/deleteInterface', interfaceController.deleteInterface);
 
+/* jsonRecords */
 router.post('/addJsonRecord', jsonRecordsController.addRecord);
 router.post('/updateJsonRecord', jsonRecordsController.updateRecord);
 router.post('/getJsonRecordByPid', jsonRecordsController.getByPid);
 router.post('/getFinalJRByPid', jsonRecordsController.getFinalByPid);
+
+/* old records */
+router.post('/paramsSave', recordsController.addRecords);
+router.post('/getIterParamsByIterId', recordsController.getRecordsByInterID);
 
 /* User */
 router.get('/isLogin', userController.isLogin);
@@ -30,20 +36,18 @@ router.post('/registe', userController.registe);
 /* Project */
 router.post('/add', projectController.addPorject);
 router.get('/getAll', projectController.getAllProjects);  
+// router.post('/getAll', function(req, res, next) {
+//     console.log(req.fields);
+//     console.log('req.body', req.body);
+// }); 
 router.post('/updateProjectById', projectController.updateProjectById);
 router.get('/deleteProjectById', projectController.deleteById); 
 
-
+/* Mock */
 router.get('/mock/:projectId/*', jsonRecordsController.responseData);
 router.post('/mock/:projectId/*', jsonRecordsController.responseData);
-// router.get('/mock/:projectId/*', function(req, res, next) {
-//     var querys = url.parse(req.url, true).query;
-//     console.log(req.url, querys, req.params.projectId);
-//     var path = '/' + req.url.substring(0, req.url.indexOf('?')).split('/').splice(3).join('/')
-//     jsonRecordsController.responseData(req.params.projectId, path);
-//     // req.url.substring(0, req.url.indexOf('?'))
-// });
 
+/* Example */
 router.post('/mock/projectId/11', function(req, res, next) {
     console.log(req);
 });
